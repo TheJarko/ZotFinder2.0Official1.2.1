8d93a32503209b4460f941461aed7829532bc5c1
Jericho <capatij@uci.edu>
2020-03-02 23:02:31 -0800

    combined unity files with 1.2

---
+++ UnityNathan/build/generated/source/buildConfig/debug/com/Cypher/IndoorNav/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Cypher.IndoorNav;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.Cypher.IndoorNav";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.Cypher.IndoorNav";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
